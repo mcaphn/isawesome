6a5acb4abc7473f25a28ce4c26ef76289b2d574d
mcaphn <mica.phan@gmail.com>
2021-04-28 11:53:07 -0400

    modal close, glassmorphism

--- js/gallery.js
+++ js/gallery.js
@@ -5,22 +5,25 @@ var expand = document.getElementById("expandedImg");
 var btn = document.getElementById("button");
 var dropdown = document.getElementById("dropdown");
 
-
 for (var i = 0; i < imgs.length; i++) {
-  imgs[i].addEventListener("click", function(event) {
+  imgs[i].addEventListener("click", function (event) {
     modal.style.display = "block";
     expand.src = event.target.src;
-  })
+  });
 }
 
 //toggle dropdown menu
 
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function () {
   dropdown.classList.toggle("show");
-})
+});
 
 //close the modal
 
-close.addEventListener("click", function() {
+close.addEventListener("click", function () {
   modal.style.display = "none";
-})
+});
+
+modal.addEventListener("click", function () {
+  modal.style.display = "none";
+});
